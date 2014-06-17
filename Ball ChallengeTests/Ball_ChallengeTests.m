594b90d8e72a0a69690309f9fae14f4b1d3cab69
Miguel Serrano <Mimi@Mimi.local>
2014-06-17 21:11:42 +0200

    Initial Commit

---
+++ Ball ChallengeTests/Ball_ChallengeTests.m
@@ -0,0 +1,34 @@
+//
+//  Ball_ChallengeTests.m
+//  Ball ChallengeTests
+//
+//  Created by Miguel Serrano on 17/06/14.
+//  Copyright (c) 2014 Miguel Serrano. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ball_ChallengeTests : XCTestCase
+
+@end
+
+@implementation Ball_ChallengeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
